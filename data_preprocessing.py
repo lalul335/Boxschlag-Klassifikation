c1401b578f5513da3d8ac9e992cbcb4305aa1895
lalul335 <raoul.doublan@stud.leuphana.de>
2024-06-18 16:41:35 +0200

    daten stressen
    Hallo, Datensätze sind wirklich anstrengende Zeitgenossen. -.-

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -7,7 +7,7 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 
 
-def extract_accelerometer_data(csv_file_path, nano=True, startpunkt=550):
+def extract_accelerometer_data(csv_file_path,startpunkt=0,  nano=True):
     # Load the CSV file into a DataFrame
     df = pd.read_csv(csv_file_path)
 
@@ -610,33 +610,28 @@ def normate_dataset_period(periodLengthInMS, samplingRateUS, dataset, interpolat
     print('Fertig, jetzt erstmal lecker Bierchen!')
     return ds_interp
 
-def plot_ds(ds, saveImage=False, imageName="no_name"):
+
+def plot_ds(ds):
     x, y, z = [], [], []
-    for ds_e in ds:
-        raws = ds_e['raws']
-        raws[0]
-        x += data_to_list2(raws, axis='x')
-        y += data_to_list2(raws, axis='y')
-        z += data_to_list2(raws, axis='z')
+    ds_ = ds.to_dict('records')
+
+    for ds_e in ds_:
+        x.append(ds_e['x'])
+        y.append(ds_e['y'])
+        z.append(ds_e['z'])
 
     t = np.arange(0, len(x))
-
     fig, ax = plt.subplots(figsize=(20, 10))
-    ax.plot(t, x, 'r.', label='x-Achse')
-    ax.plot(t, y, 'b*', label='y-Achse')
-    ax.plot(t, z, 'g^', label='z-Achse')
+    ax.plot(t, x, label='x-Achse')
+    ax.plot(t, y, label='y-Achse')
+    ax.plot(t, z, label='z-Achse')
 
-    ax.set(xlabel='Abtastwerte', ylabel='a (in m/s²)',
-           title='Rohdaten eines Upper-Cuts')
+    ax.set(xlabel='datastamps', ylabel='a (in m/s²)',
+           title='lecker Daten')
     ax.grid()
     plt.legend()
-    if saveImage:
-        if imageName.endswith('.png'):
-            plt.savefig(imageName)
-        else:
-            name = imageName + '.png'
-            plt.savefig(name)
     plt.show()
+
 
 def plot_ds_element(ds_e):
     raws = ds_e['raws']
@@ -665,3 +660,113 @@ def data_to_list(raws, axis='x'):
 
 def data_to_list2(raws, axis='x'):
     return [e[axis] for e in raws]
+
+def universal_plotter_for_all_axis(list_of_datasets,
+                                   list_of_dataset_legend_titles,
+                                   plot_title, y_axis_label, x_axis_label,
+                                   figSizeTupel=(20, 10), saveImage=False,
+                                   imageName='unknown_image.png'):
+    """Plots all axis of multiple datasets (punches) of an given array. Converting mechanism is made for the notation style of the smartPunch project.
+
+    Keyword arguments:
+        list_of_datasets                -- List of datasets (type: list)
+        list_of_dataset_legend_titles   -- List strings containing the legend titles for each dataset axis (type: list)
+        plot_title                      -- Plot title (type: string)
+        y_axis_label                    -- Y-Axis label (type: string)
+        x_axis_label                    -- X-Axis label (type: string)
+        figSizeTupel                    -- Figure size as optional parameter (type: tupel, default: (20,10))
+        saveImage                       -- If True the image is saved on the filesystem (type: boolean, default: False)
+        imageName                       -- If saveImage is True, the image is saved using the given imageName (type: string, default: "unknown_image.png")
+
+    Returns:
+        void
+    """
+
+    fig, ax = plt.subplots(figsize=figSizeTupel)
+
+    idx = 0
+
+    for curDataSet in list_of_datasets:
+        universal_label = '%(titl)s (x-axis)' % {
+            'titl': list_of_dataset_legend_titles[idx]} if list_of_dataset_legend_titles[idx] != '' else 'x-axis'
+        ax.plot(curDataSet['timestamp'].values,
+                curDataSet['x'], label=universal_label)
+        universal_label = '%(titl)s (y-axis)' % {
+            'titl': list_of_dataset_legend_titles[idx]} if list_of_dataset_legend_titles[idx] != '' else 'y-axis'
+        ax.plot(curDataSet['timestamp'].values,
+                curDataSet['y'], label=universal_label)
+        universal_label = '%(titl)s (z-axis)' % {
+            'titl': list_of_dataset_legend_titles[idx]} if list_of_dataset_legend_titles[idx] != '' else 'z-axis'
+        ax.plot(curDataSet['timestamp'].values,
+                curDataSet['z'], label=universal_label)
+        idx += 1
+
+    ax.set(xlabel=x_axis_label, ylabel=y_axis_label,
+           title=plot_title)
+    ax.grid()
+    plt.legend()
+    if saveImage:
+        if imageName.endswith('.png'):
+            plt.savefig(imageName)
+        else:
+            if "." not in imageName:
+                name = imageName+'.png'
+                plt.savefig(name)
+            else:
+                print("Error: File extension {} not allowed! See docs for more.".format(
+                    imageName.split(".")[1]))
+    plt.show()
+
+def single_plot_all_axis(dataset, plot_title, y_axis_label, x_axis_label, default_axis_legend=True, figSizeTupel=(20, 10), saveImage=False,
+                         imageName='unknown_image.png'):
+    """Plots all axis of a single dataset (punch). Converting mechanism is made for the notation style of the smartPunch project.
+
+    Keyword arguments:
+        dataset                         -- Dataset (punch) to plot (type: pandas.Dataframe)
+        default_axis_legend             -- If not False this parameter is used for the axis legend title instead of the default value (type: string, default: True)
+        plot_title                      -- Plot title (type: string)
+        y_axis_label                    -- Y-Axis label (type: string)
+        x_axis_label                    -- X-Axis label (type: string)
+        figSizeTupel                    -- Figure size as optional parameter (type: tupel, default: (20,10))
+        saveImage                       -- If True the image is saved on the filesystem (type: boolean, default: False)
+        imageName                       -- If saveImage is True, the image is saved using the given imageName (type: string, default: "unknown_image.png")
+
+    Returns:
+        void
+    """
+    fig, ax = plt.subplots(figsize=figSizeTupel)
+
+    universal_label = 'x-axis' if default_axis_legend == True else default_axis_legend[0]
+    ax.plot(dataset['timestamp'].values, dataset['x'], label=universal_label)
+    universal_label = 'y-axis' if default_axis_legend == True else default_axis_legend[1]
+    ax.plot(dataset['timestamp'].values, dataset['y'], label=universal_label)
+    universal_label = 'z-axis' if default_axis_legend == True else default_axis_legend[2]
+    ax.plot(dataset['timestamp'].values, dataset['z'], label=universal_label)
+
+    ax.set(xlabel=x_axis_label, ylabel=y_axis_label,
+           title=plot_title)
+    ax.grid()
+    plt.legend()
+    if saveImage:
+        if imageName.endswith('.png'):
+            plt.savefig(imageName)
+        else:
+            if "." not in imageName:
+                name = imageName+'.png'
+                plt.savefig(name)
+            else:
+                print("Error: File extension {} not allowed! See docs for more.".format(
+                    imageName.split(".")[1]))
+    plt.show()
+
+
+def print_usage_info():
+    print("use the functions in the notation of the following examples:")
+    print(
+        "universal_plotter_for_single_axis([ds[0],...,ds[n]],['axisToPrintForDs1','axisToPrintForDsN'],['ds[0] legend text','ds[N] legend text'],'Plot-Title','y-Axis description','y-Axis description')")
+    print(
+        "universal_plotter_for_all_axis([ds[0],...,ds[n]],['ds[0] legend text','ds[N] legend text'],'Plot-Title','y-Axis description','x-Axis description')")
+    print("single_plot_single_axis(dataset,'axisToPrint','Plot-Title','y-Axis description','x-Axis description','default_axis_legend='legend text')")
+    print("single_plot_all_axis(dataset,'Plot-Title','y-Axis description','x-Axis description','default_axis_legend='legend text')")
+
+
